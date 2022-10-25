a28ae42ee96d3eb951e4eb78d67beabd168ce1be
Diana Fontaine <47039251+difontaine@users.noreply.github.com>
2022-10-25 16:17:14 -0400

    Add files via upload

--- raw_data_processing.R
+++ raw_data_processing.R
@@ -1,21 +1,27 @@
 ### Prepping data files for NES-LTER data package -- this script manipulates the raw data to include correct column names and categories for downstream processing.
 ## Author: Diana Fontaine
-# Input: 1) raw_npp_data.csv that has data from the analysis lab; 2) light_data.csv that includes % PAR data for each station per cruise
-# Output: raw_data_table.csv that is used for downstream pp_rate calculations.
+# Input: 1) "raw_npp_data.csv" files dependent on version. These files have the raw data from the analysis lab; 2) light_data.csv that includes % PAR data for each station per cruise. Version 1: EN644 - EN649 and Version 2: EN655 - EN687
+# Output: "raw_data_table.csv" with appropriate version appended to file name. These outputs are used for downstream npp_rate calculations.
 
 
 #load libaries
 library(tidyverse)
 
-#load data
+#load light data
 light <- read_csv("light_data.csv")
-npp_raw <- read_csv("raw_npp_data.csv")
+npp_raw_vers1 <- read_csv("raw_npp_data_version1.csv")
+npp_raw_vers2 <- read_csv("raw_npp_data_version2.csv")
+
+#bind together two versions to be able to do the same thing
+npp_raw <- rbind(npp_raw_vers1, npp_raw_vers2)
+
 
 #make npp_raw cast numeric
 npp_raw$cast <- as.numeric(npp_raw$cast)
 
 #make light cast numeric
 light$cast <- as.numeric(light$cast)
+light$station <- as.character(light$station)
 
 #Merge to get light data in raw file
 npp_raw <- npp_raw %>%
@@ -26,7 +32,7 @@ npp_raw$percent_surface_irradiance <- npp_raw$percent_surface_irradiance*100
 
 #separate alternate_id column
 npp_raw <- npp_raw %>%
-  separate(alternate_sample_category, into = c("depth_category", "filter_size"), sep = "_", remove = FALSE)
+  separate(alternate_sample_ID, into = c("depth_category", "filter_size"), sep = "_", remove = FALSE)
 
 #all occurence of "GFF" under filter_size should be >0
 npp_raw$filter_size[npp_raw$filter_size=='GFF'] <- ">0"
@@ -50,8 +56,16 @@ npp_raw$filter_size[npp_raw$filter_size=='NatAbun'] <- ">0"
 npp_raw <- npp_raw %>%
   select(-date)
 
-#Save as "raw_data_table.csv"
-write_csv(npp_raw, "For_EDI/raw_data_table.csv")
+#save the two versions of the files
+npp_table_version1 <- npp_raw %>%
+  filter(cruise %in% c("EN644", "AR39B", "EN649"))
+
+npp_table_version2 <- npp_raw %>%
+  filter(!(cruise %in% c("EN644", "AR39B", "EN649")))
+
+#Save as "raw_data_table.csv"-- make sure to save the correct versions
+write_csv(npp_table_version1, "For_EDI/raw_data_table_version1.csv") 
+write_csv(npp_table_version2, "For_EDI/raw_data_table_version2.csv") 
 
 
 
